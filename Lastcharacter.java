ee98b7d8bb11ad8f377aebfd4d67eed94be0120d
Pranali Shendage <159568977+pranali-shendage@users.noreply.github.com>
2026-03-05 22:42:51 +0530

    Add files via upload

---
+++ Lastcharacter.java
@@ -0,0 +1,14 @@
+/* Q12. Write a program to print the last character of a string.
+ Input : India
+ Output : a
+*/
+
+
+public class Lastcharacter{
+public static void main(String []x){
+  
+   String s="India";
+   System.out.println(s.charAt(4));
+  
+   }
+}

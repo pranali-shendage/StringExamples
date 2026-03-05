0a9f9d8671621dbdc08414a66bb30ec224175459
Pranali Shendage <159568977+pranali-shendage@users.noreply.github.com>
2026-03-05 22:41:56 +0530

    Add files via upload

---
+++ ASCIIValue.java
@@ -0,0 +1,17 @@
+/*Q17. Find ASCII value of character
+
+Input: A
+Output: 65
+
+*/
+
+public class ASCIIValue {
+    public static void main(String[] args) {
+
+        char ch = 'A';
+
+        int ascii = ch;
+
+        System.out.println("ASCII value = " + ascii);
+    }
+}

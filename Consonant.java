0b4b14b93e7a91438ec6023542878c02ed602e3c
Pranali Shendage <159568977+pranali-shendage@users.noreply.github.com>
2026-03-06 22:20:27 +0530

    Add files via upload

---
+++ Consonant.java
@@ -0,0 +1,21 @@
+public class Consonant{
+public static void main(String []x){
+  
+   String str="Educate";
+   int count=0;
+
+  for(int i=0 ;i< str.length();i++){
+    
+     char ch= str.charAt(i);
+    
+   if(ch >='a' && ch<='z'){
+
+     if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+      
+       count ++;
+     }
+   }
+  } 
+     System.out.println(count);
+   }
+}

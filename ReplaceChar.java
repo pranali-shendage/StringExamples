744c150e123a54e153c21c002ce8cda861758d27
Pranali Shendage <159568977+pranali-shendage@users.noreply.github.com>
2026-03-27 11:41:37 +0530

    Add files via upload

---
+++ ReplaceChar.java
@@ -0,0 +1,19 @@
+/*Q18. Replace character in string
+
+Input: banana
+replace a with o
+
+Output: bonono
+
+*/
+
+public class ReplaceChar {
+    public static void main(String[] args) {
+
+        String str = "banana";
+
+        str = str.replace('a', 'o');
+
+        System.out.println(str);
+    }
+}
